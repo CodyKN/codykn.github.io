c2b86f2804f1eac8e038d44df3d91a04ba2f44c3
Cody Nguyen <36778689+CodyKN@users.noreply.github.com>
2024-11-05 14:54:56 -0600

    Move to different template

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-# Resources
-
-https://icones.js.org/
+https://astro.build
+https://tailwindcss.com
+https://www.markdownguide.org/
+https://mdxjs.com/
+https://github.com/vercel/satori
+https://pagefind.app/
+https://github.com/natemoo-re/astro-icon#readme
+https://expressive-code.com/
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,70 +1,36 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
-function removeDupsAndLowercase(list: string[]) {
-	if (!list.length) return list;
-	const lowercaseItems = list.map((str) => str.toLowerCase());
-	const uniqueItems = new Set(lowercaseItems);
-	return Array.from(uniqueItems);
-
+function removeDupsAndLowerCase(array: string[]) {
+	if (!array.length) return array;
+	const lowercaseItems = array.map((str) => str.toLowerCase());
+	const distinctItems = new Set(lowercaseItems);
+	return Array.from(distinctItems);
 }
 
-const blog = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
-	schema: () => z.object({
-		title: z.string().max(150),
-		description: z.string().max(250),
-		// Transform string to Date object
-		pubDate: z
-			.string()
-			.or(z.date())
-			.transform(val => new Date(val)),
-		updatedDate: z
-			.string()
-			.or(z.date())
-			.transform(val => val ? new Date(val) : undefined)
-			.optional(),
-		heroImage: z.object({
-			src: z.string(),
-			alt: z.string().optional(),
-		}).optional(),
-		ogImage: z.string().optional(),
-		tags: z
-			.array(z.string())
-			.default([])
-			.transform(removeDupsAndLowercase)
-			.optional(),
-		series: z.string().optional(),
-		draft: z.boolean().optional().default(false),
-		// for pinning posts
-		order: z.number().min(1).max(5).optional(),
-		// hide a post from pagination
-		hide: z.boolean().optional().default(false)
-	}),
+const post = defineCollection({
+	schema: ({ image }) =>
+		z.object({
+			coverImage: z
+				.object({
+					alt: z.string(),
+					src: image(),
+				})
+				.optional(),
+			description: z.string().min(50).max(160),
+			draft: z.boolean().default(false),
+			ogImage: z.string().optional(),
+			publishDate: z
+				.string()
+				.or(z.date())
+				.transform((val) => new Date(val)),
+			tags: z.array(z.string()).default([]).transform(removeDupsAndLowerCase),
+			title: z.string().max(60),
+			updatedDate: z
+				.string()
+				.optional()
+				.transform((str) => (str ? new Date(str) : undefined)),
+		}),
+	type: "content",
 });
 
-const project = defineCollection({
-	type: 'content',
-	schema: () => z.object({
-		title: z.string(),
-		description: z.string(),
-		pubDate: z
-			.string()
-			.or(z.date())
-			.transform(val => new Date(val)),
-		heroImage: z.object({
-			url: z.string(),
-			alt: z.string().optional()
-		}).optional(),
-		ogImage: z.string().optional(),
-		stack: z.array(z.string()).default([]).transform(removeDupsAndLowercase),
-		platform: z.string().optional(),
-		website: z.string().optional(),
-		github: z.string().optional(),
-		draft: z.boolean().optional().default(false),
-		// for pinning projects
-		order: z.number().min(1).max(5).optional()
-	})
-});
-
-export const collections = { blog, project };
+export const collections = { post };
