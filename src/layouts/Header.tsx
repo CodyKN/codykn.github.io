1c152fcd9089c2c815a6fcad1a3e0689ca1fde9d
Cody Nguyen <36778689+CodyKN@users.noreply.github.com>
2024-11-29 13:52:32 -0600

    Light mode only

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -22,7 +22,7 @@ export const Header = () => {
                 </a>
                 <nav
                     aria-label="Main menu"
-                    class={`absolute -inset-x-4 top-14 ${menuOpen() ? "flex" : "hidden"} flex-col items-end gap-y-4 rounded-md bg-bgColor/[.85] py-4 text-accent shadow backdrop-blur sm:static sm:z-auto sm:-ms-4 sm:mt-1 sm:flex sm:flex-row sm:items-center sm:divide-x sm:divide-dashed sm:divide-accent sm:rounded-none sm:bg-transparent sm:py-0 sm:shadow-none sm:backdrop-blur-none`}
+                    class={`absolute -inset-x-4 top-14 ${menuOpen() ? "flex" : "hidden"} flex-col items-end gap-y-4 rounded-md bg-bgColor/[.85] py-4 text-accent shadow backdrop-blur sm:static sm:z-auto sm:-ms-4 sm:mt-4 sm:flex sm:flex-row sm:items-center sm:rounded-none sm:bg-transparent sm:py-0 sm:shadow-none sm:backdrop-blur-none`}
                     id="navigation-menu"
                 >
                     {MenuLinks.map((link) => (
